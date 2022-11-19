9e51400fc1f50ef1a6fa134712903fb077b3d223
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-19 17:28:43 -0300

    refactor: change texts in site

--- src/components/Main/components/Biography/index.tsx
+++ src/components/Main/components/Biography/index.tsx
@@ -6,10 +6,8 @@ export function Biography() {
       <h2>Bio</h2>
       <small>Entusiasta de desenvolvimento com as melhores tecnologias!</small>
       <p>
-        Nicolas Teófilo é um desenvolvedor de software. Atualmente vivendo em
-        Ribeirão Preto, São Paulo. Um admirador de desenvolvimento e
-        compartilhamento de conhecimento! Atualmente trabalhando como
-        desenvolvedor independente, mas sempre disposto a novos desafios.
+        Nicolas Teófilo é um desenvolvedor de software. Sempre buscando se
+        atualizar na área de desenvolvimento e compartilhar seus conhecimentos!
       </p>
     </Container>
   )

511484ba61b915aa656d3c132e26b0eacae0389f
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-19 18:23:11 -0300

    feat: add 2022 projects

--- src/templates/Projects/index.tsx
+++ src/templates/Projects/index.tsx
@@ -1,5 +1,6 @@
+import { Heading } from 'components/Heading'
 import { Title } from 'components/Title'
-import { Container, Description } from './styles'
+import { Container, Description, ProjectsContainer } from './styles'
 
 type ProjectsProps = {
   title: string
@@ -14,6 +15,30 @@ export default function Projects({ title }: ProjectsProps) {
         navegar por todos os meus <strong>websites, apps e templates</strong>{' '}
         que eu já fiz. Alguns projetos estão ativos e outros talves não.
       </Description>
+      <Heading text="Todos Projetos" />
+      <Heading className="project-year" text="2022" size={1.8} />
+      <ProjectsContainer>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/nicolasteofilo/template-nextjs"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Next.Js Template
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/nicolasteofilo/my-contacts"
+              target="_blank"
+              rel="noreferrer"
+            >
+              MyContacts
+            </a>
+          </li>
+        </ul>
+      </ProjectsContainer>
     </Container>
   )
 }
--- src/templates/Projects/styles.ts
+++ src/templates/Projects/styles.ts
@@ -13,19 +13,45 @@ export const Container = styled.main`
   @media (max-width: 768px) {
     padding: 0 20px;
   }
+
+  .project-year {
+    margin-top: 3rem;
+  }
 `
 
 export const Description = styled.p`
   justify-content: center;
   margin-top: 1rem;
+  margin-bottom: 3.4rem;
   max-width: 700px;
 
   font-family: 'Biotif';
   font-size: 1.6rem;
   font-weight: normal;
+  line-height: 2;
   color: var(--gray);
 
   strong {
     color: var(--white);
+  }
+`
+export const ProjectsContainer = styled.div`
+  margin-left: 2rem;
+
+  ul {
+    li {
+      font-size: 14px;
+
+      a {
+        font-size: 16px;
+        line-height: 32px;
+        color: var(--white);
+        text-decoration: underline;
+      }
+
+      &::marker {
+        color: var(--gray);
+      }
+    }
   }
 `

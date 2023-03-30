88581bb5252b88685379f18615a6b6036c92c90d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-03-30 16:57:36 -0300

    fix: remove all components with style-components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <header className="flex justify-between items-center w-screen">
+    <header className="flex justify-between items-center w-screen mt-2">
       <div>
         <span className="font-bold italic text-3xl">NT</span>
       </div>

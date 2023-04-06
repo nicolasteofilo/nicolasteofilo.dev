a2fba09090fc394fdbe4bca2494f2f8dd5f58c59
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-04-06 10:08:21 -0300

    refactor: personalize about page with my datas

--- data/about.js
+++ data/about.js
@@ -1,15 +1,15 @@
 const items = [
   {
     jobTitle: 'Back-end Developer',
-    company: 'DotCoding',
-    companyUrl: 'https://dotcoding.com.br/',
+    company: 'Dotcoding',
+    companyUrl: 'https://Dotcoding.com.br/',
     startDate: '2023-01-01',
     location: 'Brazil - Remote',
   },
   {
     jobTitle: 'Front-end Developer',
-    company: 'DotCoding',
-    companyUrl: 'https://dotcoding.com.br/',
+    company: 'Dotcoding',
+    companyUrl: 'https://Dotcoding.com.br/',
     startDate: '2021-10-01',
     endDate: '2022-07-01',
     location: 'Brazil - Remote',

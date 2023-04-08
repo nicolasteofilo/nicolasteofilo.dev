3b67186109ea6d3f22701dceb0ca17d410bc302e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-04-08 20:10:08 -0300

    chore: update projects

--- data/projects.js
+++ data/projects.js
@@ -3,9 +3,16 @@ const items = [
     year: '2022',
     projects: [
       {
-        title: 'Next.Js Template',
-        description: 'Boilerplate for Next.Js project startup',
-        url: 'https://github.com/nicolasteofilo/template-nextjs',
+        title: 'Clean Node API',
+        description: 'REST API for polls, following Clean Architecture with TDD',
+        url: 'htps://github.com/nicolasteofilo/template-nextjs',
+        active: true,
+        icon: 'dracula-pro',
+      },
+      {
+        title: 'Won Games',
+        description: 'Virtual game store (like Steam) built 100% with NextJS, using a BaaS (Backend as a Service)',
+        url: 'https://github.com/nicolasteofilo/won-games',
         active: true,
         icon: 'dracula-pro',
       },

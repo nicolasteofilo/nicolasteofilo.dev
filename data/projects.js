83b52d26330e2ab3d1f26a2bee4a5af682577e00
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-11-12 10:05:02 -0300

    chore: add more projects

--- data/projects.js
+++ data/projects.js
@@ -3,18 +3,28 @@ const items = [
     year: '2022',
     projects: [
       {
+        title: 'CoinVault',
+        description: 'A full-stack web application for managing your cryptocurrency portfolio, built using Next.js',
+        url: 'https://github.com/nicolasteofilo/coinvault',
+        active: true,
+      },
+      {
         title: 'Clean Node API',
         description: 'REST API for polls, following Clean Architecture with TDD',
         url: 'htps://github.com/nicolasteofilo/template-nextjs',
         active: true,
-        icon: 'dracula-pro',
       },
       {
         title: 'Won Games',
         description: 'Virtual game store (like Steam) built 100% with NextJS, using a BaaS (Backend as a Service)',
         url: 'https://github.com/nicolasteofilo/won-games',
         active: true,
-        icon: 'dracula-pro',
+      },
+      {
+        title: 'Fincheck',
+        description: 'A simple and complete platform for your financial management',
+        url: 'https://github.com/nicolasteofilo/fincheck',
+        active: true,
       },
       {
         title: 'MyContacts',
@@ -28,4 +38,5 @@ const items = [
 
 const featured = [];
 
-export { items, featured  }
+export { featured, items };
+
